e85e2c2635695b5a4abd358346edf32544d491a8
81802 <81802@192.168.10.107>
2023-05-26 02:53:04 +0900

    Inisial Commit

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 名前
+		String name = "北元　蓮太";
+		// 歳
+		int age = 22;
+		// 趣味
+		String hobby = "ゲームする事";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
