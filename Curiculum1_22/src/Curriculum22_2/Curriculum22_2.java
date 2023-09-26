b4648b8ad2f7b428debff7823cc4f10df34ab75e
81802 <81802@192.168.10.140>
2023-09-27 02:11:07 +0900

    Inisial Commit

--- Curiculum1_22/src/Curriculum22_2/Curriculum22_2.java
+++ Curiculum1_22/src/Curriculum22_2/Curriculum22_2.java
@@ -5,24 +5,26 @@ import java.util.Calendar;
 
 public class Curriculum22_2 {
 	
+	// 変数を作成
 	String aisatu;
-
+	// 変数を作成
 	String susi;
-
+	// 変数を作成
 	String shurui;
 
+	// コンストラクターを定義
 	public Curriculum22_2() {
-
+		// 初期値の設定
 		this.aisatu = "こんにちは！ここは日本です！";
-
+		// 初期値の設定
 		this.susi = "この寿司はうまい！";
-
+		// 初期値の設定
 		this.shurui = "寿司は和食です";
-
+		// 出力
 		System.out.println(this.aisatu);
-
+		// 出力
 		System.out.println(this.susi);
-
+		// 出力
 		System.out.println(this.shurui);
 
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
